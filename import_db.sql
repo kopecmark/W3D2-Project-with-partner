03cdcadeb5e55cd49e74f825b68d60dcb768a9be
kopecmark <mark.kopec@gmail.com>
2018-11-06 14:34:27 -0800

    complete half easy queries

--- import_db.sql
+++ import_db.sql
@@ -34,8 +34,8 @@ CREATE TABLE replies(
   body VARCHAR(255),
 
   FOREIGN KEY (question_ref) REFERENCES questions(id),
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (parent_reply) REFERENCES replies(id)
+  FOREIGN KEY (parent_reply) REFERENCES replies(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 DROP TABLE IF EXISTS question_likes;
@@ -47,3 +47,30 @@ CREATE TABLE question_likes(
   FOREIGN KEY (question_id) REFERENCES questions(id),
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+INSERT INTO users
+VALUES (1, 'Jane', 'Smith'),
+  (2, 'John', 'Patrick'),
+  (3, 'Patrick', 'Stewart'),
+  (4, 'Joanne', 'Samsonite');
+
+INSERT INTO questions
+VALUES (1, 'Cat''s are great', 'Wow they are so amazing I can''t stop thinking about them', 1),
+  (2, 'Star Trek', 'What will Patrick Stewart be in the next series?', 3),
+  (3, 'Apple', 'Are they overpriced? The answer is yes.', 2),
+  (4, 'App Academy', 'Will they let us catch up on sleep? I sure hope so.', 1);
+
+INSERT INTO replies
+VALUES (1, 3, NULL, 1, 'Of course they are.'),
+  (2, 4, NULL, 4, 'Hells no.'),
+  (3, 4, 2, 3, 'I firmly agree with your unbiased response.');
+
+INSERT INTO question_follows
+VALUES (1, 2, 1),
+  (2, 2, 2),
+  (3, 4, 3);
+
+INSERT INTO question_likes
+VALUES (1, 1, 2),
+  (2, 3, 2),
+  (3, 4, 3);
--- questions_db.rb
+++ questions_db.rb
@@ -19,18 +19,18 @@ class Users
       WHERE users.id = ?
     SQL
 
+    return nil if user.length < 1
     Users.new(user.first)
   end
 
   def self.find_by_name(first, last)
     user = QuestionsDatabase.instance.execute(<<-SQL, first, last)
-    SELECT *
-    From users
-    WHERE user.first_name = first AND user.last_name = last
+      SELECT *
+      From users
+      WHERE user.first_name = first AND user.last_name = last
     SQL
 
     return nil if user.length < 1
-
     Users.new(user.first)
   end
 
@@ -40,4 +40,79 @@ class Users
     @last_name = options['last_name']
   end
 
+  def authored_questions
+    Questions.find_by_author_id(@id)
+  end
+
+  def authored_replies
+    Replies.find_by_user_id(@id)
+  end
+
 end
+
+
+class Questions
+  def self.find_by_author_id(author_id)
+    author = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+      SELECT * FROM questions
+      WHERE questions.author = ?
+    SQL
+
+    return nil if author.length < 1
+
+    result = []
+    author.each { |el| result << Questions.new(el) }
+    result
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @author = options['author']
+  end
+
+
+end
+class Replies
+  def self.find_by_user_id(user_id)
+    user = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+      SELECT * FROM replies
+      WHERE replies.user_id = ?
+    SQL
+
+    return nil if user.length < 1
+    result = []
+    user.each { |el| result << Replies.new(el) }
+    result
+  end
+
+  def self.find_by_question_id(question_id)
+    reply = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+    SELECT * FROM replies
+    WHERE question_ref = ?
+    SQL
+    return nil if reply.length < 1
+
+    result = []
+    reply.each { |el| result << Replies.new(el) }
+    result
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_ref = options['question_ref']
+    @parent_reply = options['parent_reply']
+    @user_id = options['user_id']
+    @body = options['body']
+  end
+
+end
+
+# p Questions.find_by_author_id(2)
+# p Replies.find_by_user_id(1)
+# p Replies.find_by_question_id(4)
+
+user = Users.find_by_id(1)
+# p user.authored_questions
+p user.authored_replies
